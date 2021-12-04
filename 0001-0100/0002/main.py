f23b7fb228fb03f7ad434685c7cc2a5ff50d276a
Micah Wood <mxw7265@rit.edu>
2021-12-04 08:52:10 -0700

    Added Problem #2 Solution

---
+++ 0001-0100/0002/main.py
@@ -0,0 +1,22 @@
+FILENAME = "input.txt"
+
+def get_input() -> int:
+    with open(FILENAME) as file:
+            input = int(file.readline())
+
+    return input
+
+def main():
+    upper_limit = get_input()
+    result = 0
+    a, b = 1, 2
+
+    while b <= upper_limit:
+        result += b
+
+        a, b = 2*b + a, 3*b + 2*a
+
+    print(f"The sum of the even-valued terms is {result}")
+
+if __name__ == "__main__":
+    main()
