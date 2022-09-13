fe88fd97773994cc2ace16bf1deb9f2f514a66f6
Micah Wood <mxw7265@rit.edu>
2022-09-13 07:52:30 -0600

    Added Problem #3 Solution

---
+++ 0001-0100/0003/main.py
@@ -0,0 +1,16 @@
+from sympy import primefactors
+
+FILENAME = "input.txt"
+
+def get_input() -> int:
+    with open(FILENAME) as file:
+        return int(file.readline())
+
+def main():
+    num = get_input()
+    result = primefactors(num)[-1]
+
+    print(f"Answer is {result}")
+
+if __name__ == "__main__":
+    main()
