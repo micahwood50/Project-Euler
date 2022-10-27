c828c65937d2345e0a1ce9c1b8a39b7a55e49291
Micah Wood <mxw7265@rit.edu>
2022-10-27 09:20:44 -0600

    Added Problem #6 Solution
    And also tidied #5.

--- 0001-0100/0005/main.py
+++ 0001-0100/0005/main.py
@@ -1,9 +1,9 @@
 def isDivisibleByAll20(n: int) -> bool:
-  return all(n%i == 0 for i in range(2, 21))
+    return all(n%i == 0 for i in range(2, 21))
 
 def main():
     result = 2520
-    
+
     while not isDivisibleByAll20(result):
         result += 20
 
---
+++ 0001-0100/0006/main.py
@@ -0,0 +1,10 @@
+def difference(n: int) -> int:
+    return (n*(n+1)*(3*n+2)*(n-1))//12
+
+def main():
+    result = difference(100)
+
+    print(f"Answer is {result}")
+
+if __name__ == "__main__":
+    main()
