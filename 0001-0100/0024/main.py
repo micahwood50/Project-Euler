f4f0d47f2bc5d4d0af7093a459fce3bc42227aea
Micah Wood <mxw7265@rit.edu>
2022-11-09 10:58:43 -0700

    Added Problem #24 Solution

---
+++ 0001-0100/0024/main.py
@@ -0,0 +1,13 @@
+from itertools import permutations
+
+
+def main():
+    for i, n in enumerate(permutations(range(10)), start = 1):
+        if i == 1_000_000:
+            result = "".join(str(d) for d in n)
+            print(f"Answer is {result}")
+            break
+
+
+if __name__ == "__main__":
+    main()
