23e4c646b3284eacbfe563bdc586f90be14492ec
Micah Wood <mxw7265@rit.edu>
2023-03-19 15:06:37 -0600

    Improved Problem #45 Solution

--- 0001-0100/0045/main.py
+++ 0001-0100/0045/main.py
@@ -1,7 +1,6 @@
 def main():
-    Tn = Pn = Hn = 40755
+    Pn = Hn = 40755
 
-    n_T = 285
     n_P = 165
     n_H = 143
 
@@ -9,15 +8,11 @@ def main():
         n_H += 1
         Hn += 4 * n_H - 3
 
-        while Hn > Tn:
-            n_T += 1
-            Tn += n_T
-
         while Hn > Pn:
             n_P += 1
             Pn += 3 * n_P - 2
 
-        if Tn == Pn == Hn:
+        if Pn == Hn:
             print(f"Answer is {Hn}")
             return
 
