1174bb38997b0bc971c55fa3e2023d78ef80ce9d
Micah Wood <mxw7265@rit.edu>
2023-04-26 02:04:01 -0600

    Reformatted Problem #800 Solution

--- 0701-0800/0800/main.py
+++ 0701-0800/0800/main.py
@@ -1,10 +1,11 @@
 from math import log
 from sympy import sieve
 
+
 def main():
     N = 800800
-    upper_bound = N*log(N)
-    primes_list = list(sieve.primerange(1, 20*N))
+    upper_bound = N * log(N)
+    primes_list = list(sieve.primerange(1, 20 * N))
 
     result = 0
     dj = 1
@@ -13,7 +14,7 @@ def main():
         p = primes_list[i]
         q = primes_list[i - dj]
 
-        while p*log(q) + q*log(p) > upper_bound and dj <= i:
+        while p * log(q) + q * log(p) > upper_bound and dj <= i:
             dj += 1
             q = primes_list[i - dj]
 
